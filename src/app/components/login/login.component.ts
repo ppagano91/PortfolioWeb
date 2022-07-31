d063e408464598b0fa11fa8853ed66495fbd5225
Patricio Pagano <pagano.patricio@gmail.com>
2022-07-31 00:56:31 -0300

    estilos + formBuilder & formGroup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.form=this.formBuilder.group({
+      email:["",[Validators.required, Validators.email]],
+      password:["",[Validators.required,Validators.minLength(8)]],
+      deviceInfo:this.formBuilder.group({
+      deviceId: ["17867868768"],
+      deviceType:["DEVICE_TYPE_ANDROID"],
+      notificationToken:["67657575eececc34"]
+    })
+  })
+  }
 
   ngOnInit(): void {
+  }
+
+  get Email(){
+    return this.form.get('email')
+  }
+
+  get Password(){
+    return this.form.get('password')
   }
 
 }
