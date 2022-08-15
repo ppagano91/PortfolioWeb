d7df40cba63c91e8adbd105414dcb22a71675256
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-14 23:57:54 -0300

    Modelo Formación Académica creado

---
+++ src/app/model/formacion-academica.ts
@@ -0,0 +1,24 @@
+export class FormacionAcademica {
+    id?: Integer;
+    titulo: String;
+    descripcion: String;
+    fechaInicio: Date;
+    fechaFin: Date;
+    estado: String;
+    link: String;
+    image: String;
+
+    constructor(titulo:String, descripcion: String, fechaInicio: Date, fechaFin: Date, estado: String, link: String, image: String){
+        this.titulo=titulo;
+        this.descripcion=descripcion;
+        this.fechaInicio=fechaInicio;
+        this.fechaFin=fechaFin;
+        this.estado=estado;
+        this.link=link;
+        this.image=image;
+
+    }
+
+
+
+}
