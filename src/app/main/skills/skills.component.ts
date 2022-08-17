f22c33501ef1ad97fbf5458accf2dba51dbd2e00
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-17 00:14:25 -0300

    Habilidades Ok - falta funcionalidad botones y lograr utilizar el circle-progress

--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Tecnologia } from 'src/app/model/tecnologia';
+import { TecnologiaService } from 'src/app/services/tecnologia/tecnologia.service';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
+  public tecnologias:Tecnologia[]=[];  
+  /*
+  Debería hacer 3 llamadas a la api o funciones, para separar las habilidades en Softwares, Lenguajes y otras habilidades
+  No funciona el ng-circle-progress
+  */
+  constructor(private tecnologiaService: TecnologiaService) { }
 
   ngOnInit(): void {
+    this.obtenerTodaTecnologias();
+  }
+
+  obtenerTodaTecnologias():void{
+    this.tecnologiaService.getTecnologias().subscribe(data=>{
+
+      console.log("data\n",typeof(data[0].nivel))
+      this.tecnologias=data;
+    })
   }
 
 }
--- src/app/services/tecnologia/tecnologia.service.ts
+++ src/app/services/tecnologia/tecnologia.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tecnologia } from 'src/app/model/tecnologia';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TecnologiaService {
-
-  constructor() { }
+  private url:String="http://localhost:8080/tecnologia/"
+  constructor(private http:HttpClient) {}
+  getTecnologias():Observable<Tecnologia[]>{
+    return this.http.get<Tecnologia[]>(this.url+"ver/tecnologias");
+  }
 }
