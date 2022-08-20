79d085055c1128a717364a23829ec7bf967025e1
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-20 01:41:12 -0300

    nuevos atributos empresa y tipoJornada

--- src/app/model/experiencia-laboral.ts
+++ src/app/model/experiencia-laboral.ts
@@ -6,13 +6,17 @@ export class ExperienciaLaboral {
     fechaFin: Date;
     esTrabajoActual: Boolean;
     tipoEmpleo: String;
+    empresa:String;
+    tipoJornada:String;
 
-    constructor(nombre: String,descripcion: String, fechaInicio: Date, fechaFin: Date, esTrabajoActual: Boolean, tipoEmpleo: String){
+    constructor(nombre: String,descripcion: String, fechaInicio: Date, fechaFin: Date, esTrabajoActual: Boolean, tipoEmpleo: String, empresa:String,tipoJornada:String){
         this.nombre=nombre;
         this.descripcion=descripcion;
         this.fechaInicio=fechaInicio;
         this.fechaFin=fechaFin;
         this.esTrabajoActual=esTrabajoActual;
         this.tipoEmpleo=tipoEmpleo;
+        this.empresa=empresa;
+        this.tipoJornada=tipoJornada;
     }
 }
