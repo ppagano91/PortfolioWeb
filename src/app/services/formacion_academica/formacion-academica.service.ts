039ec16aa4005b806dea46bf100cff01a3eed564
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-21 22:21:47 -0300

    Servicios de CRUD creados

--- src/app/services/formacion_academica/formacion-academica.service.ts
+++ src/app/services/formacion_academica/formacion-academica.service.ts
@@ -15,4 +15,24 @@ export class FormacionAcademicaService {
   public getEducaciones(): Observable<FormacionAcademica[]>{
     return this.http.get<FormacionAcademica[]>(this.url+"ver/educaciones")
   }
+
+  public listEducation(): Observable<FormacionAcademica[]>{
+    return this.http.get<FormacionAcademica[]>(this.url+"get/formacionacademica")
+  }
+
+  public detailEducation(id:number):Observable<FormacionAcademica>{
+    return this.http.get<FormacionAcademica>(this.url+`get/${id}`)
+  }
+
+  public saveEducation(educacion:FormacionAcademica):Observable<any>{
+    return this.http.post<any>(this.url+"create",educacion)
+  }
+
+  public updateEducation(id:number, educacion:FormacionAcademica):Observable<any>{
+    return this.http.put<any>(this.url+`update/${id}`,educacion)
+  }
+
+  public deleteEducation(id:number):Observable<any>{
+    return this.http.delete<any>(this.url+`remove/${id}`)
+  }
 }
