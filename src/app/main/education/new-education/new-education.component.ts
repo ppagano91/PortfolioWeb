5ea37c8eb6ff52dab0dca3c0ab1d698535b5aee8
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-22 19:37:57 -0300

    Función onCreate() creada

--- src/app/main/education/new-education/new-education.component.ts
+++ src/app/main/education/new-education/new-education.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormacionAcademica } from 'src/app/model/formacion-academica';
+import { FormacionAcademicaService } from 'src/app/services/formacion_academica/formacion-academica.service';
 
 @Component({
   selector: 'app-new-education',
@@ -6,8 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-education.component.css']
 })
 export class NewEducationComponent implements OnInit {
+  public formData:FormacionAcademica={
+    titulo:'',
+    descripcion:'',
+    institucion:'',
+    fechaInicio:null,
+    fechaFin:null,
+    estado:'',
+    link:'',
+    image:'',
+  }
 
-  constructor() { }
+  constructor(
+    private educacionService: FormacionAcademicaService,
+    private router: Router,
+  ) { }
+
+  onCreate():void{
+    const nuevaEducacion = this.formData;
+    this.educacionService.saveEducation(nuevaEducacion).subscribe(
+      data=>{
+        this.router.navigate(['']);
+      },
+      error=>{
+        this.router.navigate(['']);
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
