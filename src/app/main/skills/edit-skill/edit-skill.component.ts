d12238461e73f66e64d3bcf403fc7def63a761f0
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-23 02:16:48 -0300

    Función onUpdate() creada

--- src/app/main/skills/edit-skill/edit-skill.component.ts
+++ src/app/main/skills/edit-skill/edit-skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tecnologia } from 'src/app/model/tecnologia';
+import { TecnologiaService } from 'src/app/services/tecnologia/tecnologia.service';
 
 @Component({
   selector: 'app-edit-skill',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-skill.component.css']
 })
 export class EditSkillComponent implements OnInit {
+  public formData: Tecnologia={
+    nombre:"",
+    descripcion:"",
+    url:"",
+    foto:"",
+    nivel:0,
+    tipo:"",
 
-  constructor() { }
+  }
+
+  constructor(
+    private tecnologiaService: TecnologiaService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.tecnologiaService.detailSkill(id).subscribe(
+      data=>{
+        this.formData=data;
+      }, error=>{
+        alert("Error al modificar Tecnología en ngOnInit")
+        this.router.navigate(['']);
+      }
+    )
+  }
+  onUpdate():void{
+    const id=this.activatedRoute.snapshot.params['id'];
+    this.tecnologiaService.updateSkill(id,this.formData).subscribe(
+      data=>{
+        this.router.navigate(['']);
+      }, error=>{
+        alert("Error al modificar proyecto en onUpdate");
+        this.router.navigate(['']);
+      }
+    )
   }
 
 }
