8eb0f8523db83fb6657b8ea9e8a87ebf086a2b2a
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-23 00:44:19 -0300

    función onUpdate() creada

--- src/app/main/projects/edit-project/edit-project.component.ts
+++ src/app/main/projects/edit-project/edit-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Proyecto } from 'src/app/model/proyecto';
+import { ProyectoService } from 'src/app/services/proyecto/proyecto.service';
 
 @Component({
   selector: 'app-edit-project',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-project.component.css']
 })
 export class EditProjectComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public formData: Proyecto={
+    titulo:"",
+    descripcion:"",
+    fecha:null,
+    foto:"",
+    url:""
   }
 
+  constructor(
+    private proyectoService: ProyectoService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.proyectoService.detailProject(id).subscribe(
+      data=>{
+        this.formData=data;
+      }, error=>{
+        alert("Error al modificar proyecto en ngOnInit")
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  onUpdate():void{
+    const id=this.activatedRoute.snapshot.params['id'];
+    this.proyectoService.updateProject(id,this.formData).subscribe(
+      data=>{
+        this.router.navigate(['']);
+      }, error=>{
+        alert("Error al modificar proyecto en onUpdate");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
