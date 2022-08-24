4be39dffd9bb2827fcf08ee3e1b2f8804c93def6
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-24 01:58:06 -0300

    lógica login con localstorage ok

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  form:FormGroup;
+  form: FormGroup;
+  isLogged: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form=this.formBuilder.group({
-      email:["",[Validators.required, Validators.email]],
-      password:["",[Validators.required,Validators.minLength(8)]],
-      deviceInfo:this.formBuilder.group({
-      deviceId: ["17867868768"],
-      deviceType:["DEVICE_TYPE_ANDROID"],
-      notificationToken:["67657575eececc34"]
-    })
-  })
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+    ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      //   deviceInfo:this.formBuilder.group({
+      //   deviceId: ["17867868768"],
+      //   deviceType:["DEVICE_TYPE_ANDROID"],
+      //   notificationToken:["67657575eececc34"]
+      // })
+    });
   }
 
   ngOnInit(): void {
+    console.log('this.form: ', this.form);
   }
 
-  get Email(){
-    return this.form.get('email')
+  get Email() {
+    return this.form.get('email');
   }
 
-  get Password(){
-    return this.form.get('password')
+  get Password() {
+    return this.form.get('password');
   }
 
+  login(): void {    
+    if (typeof Storage !== undefined) {      
+      if (
+        this.form.value.email == 'argentina@programa.com' &&
+        this.form.value.password == 'argentinaprograma123'
+      ) {
+        localStorage.clear();
+        this.isLogged = true;
+        localStorage.setItem('email', 'argentina@programa.com');
+        localStorage.setItem('password', 'argentinaprograma123');
+        localStorage.setItem('isLogged', 'true');
+        this.router.navigate(['portfolio'])
+      } else if (
+        this.form.value.email !== 'argentina@programa.com' &&
+        this.form.value.password == 'argentinaprograma123'
+      ) {
+        alert('El email es incorrecto');
+        // LocalStorage no soportado en este navegador
+      } else if (
+        this.form.value.email == 'argentina@programa.com' &&
+        this.form.value.password !== 'argentinaprograma123'
+      ) {
+        this.isLogged = false;
+        alert('El password es incorrecto');
+      } else {
+        this.isLogged = false;
+        alert('El email y el password son incorrectos');
+      }
+    }
+  }
 }
