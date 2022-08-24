d4800d54bbf4bf5d5e47b2ea5c5231111b630a26
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-24 01:58:38 -0300

    botones login y logout creados

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public isLogged: boolean=false
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    this.isLogged=(localStorage.getItem("isLogged")=="true")
+  }
+
+  logout():void{    
+    localStorage.removeItem("isLogged");
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.reload()
+    // this.route.navigate(['portfolio'])
+
+    
   }
 
 }
