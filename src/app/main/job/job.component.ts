d8a540023c39a5a2047b2ad32c9e76190685ce25
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-27 02:00:56 -0300

    Correcciones de los eventos en los botones

--- src/app/main/job/job.component.ts
+++ src/app/main/job/job.component.ts
@@ -5,58 +5,76 @@ import { ExperienciaLaboralService } from 'src/app/services/experiencia_laboral/
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
-  styleUrls: ['./job.component.css']
+  styleUrls: ['./job.component.css'],
 })
 export class JobComponent implements OnInit {
-  trabajos:ExperienciaLaboral[]=[];
-  trabajo: ExperienciaLaboral = new ExperienciaLaboral("","",new Date, new Date,false,"","","");
-  public isLogged: boolean=false;
+  trabajos: ExperienciaLaboral[] = [];
+  trabajo: ExperienciaLaboral = new ExperienciaLaboral(
+    '',
+    '',
+    new Date(),
+    new Date(),
+    false,
+    '',
+    '',
+    ''
+  );
+  public isLogged: boolean = false;
 
-  modalSwitch:boolean;
+  modalAddJobSwitch: boolean;
+  modalEditJobSwitch: boolean;
 
   // Agregar TokenService cuando se haga
   constructor(private experienciaService: ExperienciaLaboralService) {}
 
   ngOnInit(): void {
     this.obtenerTodaExperiencaLaboral();
-    this.isLogged=(localStorage.getItem("isLogged")=="true")
+    this.isLogged = localStorage.getItem('isLogged') == 'true';
     // this.cargarExperiencia();
-    this.experienciaService.$modal.subscribe(data=>
-      this.modalSwitch=data)
+    this.experienciaService.$modalAddJob.subscribe(
+      (data) => (this.modalAddJobSwitch = data)
+    );
+
+    this.experienciaService.$modalEditJob.subscribe(
+      (data) => (this.modalEditJobSwitch = data)
+    );
   }
 
-  openModal(){
-    this.modalSwitch=true;
-
+  openAddJobModal() {
+    this.modalAddJobSwitch = true;
   }
 
-  obtenerTodaExperiencaLaboral():void{
-    this.experienciaService.getExperiencias().subscribe(data=>{
-      data.forEach(element => {
-        element.fechaInicio=new Date(element.fechaInicio);
-        element.fechaFin=new Date(element.fechaFin);
-             
-      });      
+  openEditJobModal() {
+    this.modalEditJobSwitch = true;
+  }
+
+  obtenerTodaExperiencaLaboral(): void {
+    this.experienciaService.getExperiencias().subscribe((data) => {
+      data.forEach((element) => {
+        element.fechaInicio = new Date(element.fechaInicio);
+        element.fechaFin = new Date(element.fechaFin);
+      });
       // console.log("Fecha\n",data[0].fechaInicio.toLocaleString("es-ES", { month: "long" }));
-      this.trabajos=data;
-    })
+      this.trabajos = data;
+    });
   }
 
-  cargarExperiencia():void{
-    this.experienciaService.listExperience().subscribe(data=>this.trabajos=data)
+  cargarExperiencia(): void {
+    this.experienciaService
+      .listExperience()
+      .subscribe((data) => (this.trabajos = data));
   }
 
-  onRemove(id?:number){
-    if(id!=undefined){
+  onRemove(id?: number) {
+    if (id != undefined) {
       this.experienciaService.deleteExperience(id).subscribe(
-        data=>{          
-          this.obtenerTodaExperiencaLaboral();          
-        }, err=>{
-          alert("No se pudo borrar la experiencia")
+        (data) => {
+          this.obtenerTodaExperiencaLaboral();
+        },
+        (err) => {
+          alert('No se pudo borrar la experiencia');
         }
-
-      )
+      );
     }
   }
-
 }
