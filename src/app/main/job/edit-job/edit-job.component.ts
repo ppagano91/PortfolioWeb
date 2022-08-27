13950e5a0a6e5b09cc166ddce7993f8bbf2b12aa
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-27 02:00:08 -0300

    nuevo modal para editJob

--- src/app/main/job/edit-job/edit-job.component.ts
+++ src/app/main/job/edit-job/edit-job.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ExperienciaLaboral } from 'src/app/model/experiencia-laboral';
 import { ExperienciaLaboralService } from 'src/app/services/experiencia_laboral/experiencia-laboral.service';
@@ -9,6 +9,7 @@ import { ExperienciaLaboralService } from 'src/app/services/experiencia_laboral/
   styleUrls: ['./edit-job.component.css'],
 })
 export class EditJobComponent implements OnInit {
+  @Input() id: number;
   public formData: ExperienciaLaboral = {
     nombre: '',
     descripcion: '',
@@ -28,7 +29,8 @@ export class EditJobComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const id= this.activatedRouter.snapshot.params['id'];
+    // const id= this.activatedRouter.snapshot.params['id'];
+    const id =this.id;
     this.experienciaService.detailExperience(id).subscribe(
       data=>{
         this.formData=data
@@ -41,14 +43,19 @@ export class EditJobComponent implements OnInit {
   }
 
   onUpdate():void{
-    const id= this.activatedRouter.snapshot.params['id'];
+    // const id= this.activatedRouter.snapshot.params['id'];
+    const id = this.id;
     this.experienciaService.updateExperience(id, this.formData).subscribe(
       data=>{
-        this.router.navigate(['']);
+        window.location.reload();
       },error=>{
         alert("Error al modificar experiencia en onUpdate");
         this.router.navigate(['']);
       }
     )
+  }
+
+  closeModal(){
+    this.experienciaService.$modalEditJob.emit(false);
   }
 }
