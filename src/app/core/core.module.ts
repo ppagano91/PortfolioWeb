4f7977fed784d06ef76b5fdff5bffbfaa4664f48
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-26 22:03:27 -0300

    ModalComponent agregado a declarations y exports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
 import { RouterModule } from '@angular/router';
+import { ModalComponent } from '../components/modal/modal.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HeroComponent],
+  declarations: [HeaderComponent, FooterComponent, HeroComponent, ModalComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent, HeroComponent],
+  exports: [HeaderComponent, FooterComponent, HeroComponent, ModalComponent],
 })
 export class CoreModule {}
