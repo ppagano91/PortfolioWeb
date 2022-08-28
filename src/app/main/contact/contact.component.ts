57d692b5f6e7e605493ec9c6b6bf7ec10ead9fad
Patricio Pagano <pagano.patricio@gmail.com>
2022-08-28 11:09:05 -0300

    Botón Descargar CV ok

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -5,14 +5,34 @@ import { PersonaService } from 'src/app/services/persona/persona.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-  persona:Persona = new Persona("","","","","","","",new Date("1991-07-10"),"","","","");
-  constructor(private personaService: PersonaService) { }
+  persona: Persona = new Persona(
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    new Date('1991-07-10'),
+    '',
+    '',
+    '',
+    ''
+  );
+  constructor(private personaService: PersonaService) {}
 
   ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data=>{this.persona=data})
+    this.personaService.getPersona().subscribe((data) => {
+      this.persona = data;
+    });
   }
 
+  alertMessage() {
+    alert(
+      'Por el momento esta funcionalidad no está habilitada.\n Te invito a ver mi CV con el link "Descarga mi CV"'
+    );
+  }
 }
